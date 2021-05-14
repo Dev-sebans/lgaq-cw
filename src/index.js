2766707f5cd362607483105f1433a287b54dc14e
Sebastian Devassy <sebastiandevassy@Sebastians-MacBook-Pro.local>
2021-05-15 08:29:48 +1200

    Added form functionality and processed data using context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { UserProvider } from "./UserContext";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </UserProvider>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { UserContext } from "../UserContext";
+
+import { ToWords } from "to-words";
+
 const Confirmation: React.FC<any> = () => {
+  const userData = useContext(UserContext);
+
+  const AmountNumber = parseInt(userData?.Amount);
+
+  console.log(AmountNumber);
+
+  const toWords = new ToWords({
+    localeCode: "en-US",
+    converterOptions: {
+      currency: true,
+      ignoreDecimal: false,
+      ignoreZeroCurrency: false,
+    },
+  });
+
+  let AmountToWords = toWords.convert(AmountNumber, { currency: true });
+
   return (
     <main>
-        <section className="section section--dashboard">
-      <Container>
-      <Row>
+      <section className="section section--dashboard">
+        <Container>
+          <Row>
             <Col>
               <h2 className="section__title">Confirmation</h2>
             </Col>
           </Row>
-        <Row>
+          <Row>
             <Col>
-        <div className="panel panel--success">
-        <div className="panel__header">
-            <h5 className="panel__header-title">Cheque Issued</h5>
-            </div>
-              <div className="panel__body">
-                <p className="panel__body-text">Pay to<strong>Name</strong></p>
+              <div className="panel panel--success">
+                <div className="panel__header">
+                  <h5 className="panel__header-title">Cheque Issued</h5>
+                </div>
+                <div className="panel__body">
+                  <p className="panel__body-text">
+                    Pay to<strong>{userData.FullName}</strong>
+                  </p>
+                </div>
+                <div className="panel__footer">
+                  <p className="panel__footer-text">
+                    Amount in words <strong>{AmountToWords}</strong>
+                  </p>
+                </div>
               </div>
-              <div className="panel__footer">
-              <p className="panel__footer-text">Amount in words <strong>Amount in words</strong></p>
-              </div>
-            </div>
             </Col>
-        </Row>{" "}
-      </Container>
+          </Row>{" "}
+        </Container>
       </section>
     </main>
   );
